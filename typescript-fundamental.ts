7d054820ebf362b7a16664d51109cfe436950419
nghia-code1805 <you@example.com>
2019-12-17 15:29:12 +0700

    ok:

---
+++ typescript-fundamental.ts
@@ -0,0 +1,13 @@
+function main() {
+    var x=5;
+    console.log(x);
+    var x=10;
+    console.log(x);
+
+    // let yt=55;
+    // console.log(yt);
+    // let yt=100;
+    // console.log(yt);
+
+}
+main();
